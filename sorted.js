02cf8bb5d384d2d17ebb205e5fae50bebbe1a4c3
nyambujoy <joyliznyambura@gmail.com>
2024-06-16 13:29:06 +0300

    removed duplicates

---
+++ sorted.js
@@ -0,0 +1,48 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    const sorted = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            sorted.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            sorted.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    return sorted.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function removeDupes(arr) {
+    if (arr.length === 1) {
+        return arr
+    }
+    const unique = [arr[0]]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1]) {
+            unique.push(arr[i])
+        }
+    }
+    return unique
+}
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// console.log(mergeSort(array));
+let newlySort = (mergeSort(array))
+function handleDupes(arr) {
+    return [...new Set(arr)]
+}
+console.log(handleDupes(newlySort))
